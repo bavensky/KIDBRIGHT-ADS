2073749d90fdbe2c79d63675ab39b7de8fa41f76
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-09-06 16:23:54 +0700

    add ADS sensor and LCD I2C

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3f, 16, 2);
+
+// Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  
+  Serial.println("Single-ended readings from AIN0 with >3.0V comparator");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+  Serial.println("Comparator Threshold: 1000 (3.000V)");
+  
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  //ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+   ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  
+  ads.begin();
+  
+  // Setup 3V comparator on channel 0
+  ads.startComparator_SingleEnded(0, 1000);
+
+
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Hello, world!");
+}
+
+void loop(void) 
+{
+  int16_t adc0;
+
+  // Comparator will only de-assert after a read
+  adc0 = ads.getLastConversionResults();
+  float voltage = adc0 * (3.3 / 2047.0);
+  Serial.print("AIN0: "); 
+  Serial.print(adc0);
+  Serial.print("\tVoltage: "); 
+  Serial.println(voltage);
+  delay(100);
+
+  lcd.setCursor(0, 0);
+  lcd.print("KIDBRIGHT ADS");
+  lcd.setCursor(0, 1);
+  lcd.print("Voltage = ");
+  lcd.print(voltage);
+  lcd.print("  ");
+}
